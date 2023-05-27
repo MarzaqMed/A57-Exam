55378b3dbcc4d5d8e69e3b5123ef7827ed145f56
MarzaqMed <marzaq6@gmail.com>
2023-05-26 23:26:43 -0400

    pointure_CML

--- pointure.py
+++ pointure.py
@@ -1,16 +1,11 @@
-# The data set used in this example is from http://archive.ics.uci.edu/ml/datasets/Wine+Quality
-# P. Cortez, A. Cerdeira, F. Almeida, T. Matos and J. Reis.
-# Modeling wine preferences by data mining from physicochemical properties. In Decision Support Systems, Elsevier, 47(4):547-553, 2009.
-# ! pip install --user mlflow
-
 import logging
 import warnings
 
-import mlflow.sklearn
 import numpy as np
 import pandas as pd
 from sklearn import metrics
 from sklearn import preprocessing
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 
@@ -29,43 +24,83 @@ if __name__ == "__main__":
         logger.exception(
             "Unable to download training & test CSV, check your internet connection. Error: %s", e)
 
+
+
+    # Charger le data
+    df = pd.read_csv('pointure.data')
+    # df
+
+    # df.describe()
+
+    # pretraitement des donnees
+
+
     label_encoder = preprocessing.LabelEncoder()
     input_classes = ['masculin', 'féminin']
     label_encoder.fit(input_classes)
 
     # transformer un ensemble de classes
     encoded_labels = label_encoder.transform(df['Genre'])
-    # print(encoded_labels)
+    print(encoded_labels)
     df['Genre'] = encoded_labels
 
-    # Split the data into training and test sets. (0.75, 0.25) split.
+    # df
+
+    # Séparation des features et de la variable cible
     X = df.iloc[:, lambda df: [1, 2, 3]]
     y = df.iloc[:, 0]
 
-    # The predicted column is "quality" which is a scalar from [3, 9]
+    # Train/test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=44)
 
-    # Note de
+    # print (X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-    mlflow.set_tracking_uri("http://localhost:5000")
-    mlflow.set_experiment(experiment_name='experiment2')
+    # Définir le modèle
+    gnb = GaussianNB()
 
-    with mlflow.start_run():
-        gnb = GaussianNB()
-        gnb.fit(X_train, y_train)
-        y_naive_bayes1 = gnb.predict(X_train)
+    # Entraînement du modèle
 
-        accuracy = metrics.accuracy_score(y_train, y_naive_bayes1)
-        print("Accuracy du modele Naive Bayes predit: " + str(accuracy))
+    gnb.fit(X_train, y_train)
 
-        recall_score = metrics.recall_score(y_train, y_naive_bayes1)
-        print("recall score du modele Naive Bayes predit: " + str(recall_score))
+    # EVALUATION SUR LE TRAIN
+    y_naive_bayes1 = gnb.predict(X_train)
+    print("Number of mislabeled points out of a total 0%d points : 0%d" % (
+    X_train.shape[0], (y_train != y_naive_bayes1).sum()))
 
-        f1_score = metrics.f1_score(y_train, y_naive_bayes1)
-        print("F1 score du modele Naive Bayes predit: " + str(f1_score))
+    accuracy = metrics.accuracy_score(y_train, y_naive_bayes1)
+    print("Accuracy du modele Naive Bayes predit: " + str(accuracy))
 
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("recall_score", recall_score)
-        mlflow.log_metric("f1_score", f1_score)
+    recall_score = metrics.recall_score(y_train, y_naive_bayes1)
+    print("recall score du modele Naive Bayes predit: " + str(recall_score))
 
-        mlflow.sklearn.log_model(gnb, "model")
+    f1_score = metrics.f1_score(y_train, y_naive_bayes1)
+    print("F1 score du modele Naive Bayes predit: " + str(f1_score))
+
+    d = {'Taille(cm)': [183], 'Poids(kg)': [59], 'Pointure(cm)': [20]}
+    dfToPredict = pd.DataFrame(data=d)
+    # dfToPredict
+
+    yPredict = gnb.predict(dfToPredict)
+    print('La classe predite est : ', yPredict)
+
+    y_pred = gnb.predict(X_test)
+
+    # Évaluation
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    r2 = r2_score(y_test, y_pred)
+
+    print("Accuracy: {}".format(accuracy))
+    print("recall score: {}".format(recall_score))
+    print("F1 score: {}".format(f1_score))
+
+    # Écrire les scores dans un fichier
+    with open("metrics.txt", 'w') as outfile:
+        outfile.write("Accuracy: {}".format(accuracy))
+        outfile.write("recall score: {}\n".format(recall_score))
+        outfile.write("F1 score: {}\n".format(f1_score))
+
+
+
+
+
